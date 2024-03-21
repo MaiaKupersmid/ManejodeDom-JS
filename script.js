f5fc3c6b77aa06451bbc9bce1a72baea45d18a77
JULIETA IZBICKI <47350196@ort>
2024-03-21 09:03:03 -0300

    casi fin

--- script.js
+++ script.js
@@ -2,22 +2,50 @@ const Validar = (id) => {
     const nota = document.getElementById(id);
     let ok2 = false;
 
-    if (parseInt(nota.value) > 0 &&
-     parseInt(nota.value)<= 10) {
+    if (parseInt(nota.value) > 0 && parseInt(nota.value)<= 10) {
         nota.style.background = "green";        
         ok2 = true;
     } else {
         nota.style.background = "red";        
     }
-};
-/*
-const Promedio = (mate, lengua, efsi) => {
-    const prom = (mate+lengua+efsi)/3
+}
+
+const obtenerValores = (id) =>{
+    return parseInt(document.getElementById(id).value)
+}
+
+const Promedio = () => {
+    let notas = {
+        matematica : obtenerValores("mate"),
+        lengua : obtenerValores("lengua"),
+        efsi : obtenerValores("efsi"),
+    }
+    let prom = (notas.matematica + notas.lengua + notas.efsi)/3
     alert(prom)
 }
 
-const Mayor = () => {
+const Mayor = () => {    
+    let notas = {
+        Matematica : obtenerValores("mate"),
+        Lengua : obtenerValores("lengua"),
+        EFSI : obtenerValores("efsi"),
+    }
 
+    let mayor = Math.max(...Object.values(notas));
+    let resultado = [];
+    for(let materia in notas){
+        if(notas[materia] == mayor){
+            resultado.push(materia);
+        }
+    }
+    alert(resultado)
 }
 
-*/
+const mostrarResultados = () =>{
+    let notas = {
+        matematica : obtenerValores("mate"),
+        lengua : obtenerValores("lengua"),
+        efsi : obtenerValores("efsi"),
+    }
+    getElementById("result")
+}
