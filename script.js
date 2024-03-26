640758ef3175f702cc9dde7a104b65af82ebdc81
JULIETA IZBICKI <47350196@ort>
2024-03-26 09:01:41 -0300

    fin

--- script.js
+++ script.js
@@ -21,7 +21,13 @@ const Promedio = () => {
         efsi : obtenerValores("efsi"),
     }
     let prom = (notas.matematica + notas.lengua + notas.efsi)/3
-    alert(prom)
+    document.getElementById(promedio)
+    if (prom >= 6) {
+        promedio.style.background = "green";        
+    } else {
+        promedio.style.background = "red";        
+    }
+    return prom;
 }
 
 const Mayor = () => {    
@@ -38,7 +44,13 @@ const Mayor = () => {
             resultado.push(materia);
         }
     }
-    alert(resultado)
+    document.getElementById(alta)
+    for(let materia in notas){
+        if(notas[materia] == mayor){
+            alta.style.background = "blue";
+        }
+    }
+    return resultado;
 }
 
 const mostrarResultados = () =>{
@@ -47,5 +59,14 @@ const mostrarResultados = () =>{
         lengua : obtenerValores("lengua"),
         efsi : obtenerValores("efsi"),
     }
-    getElementById("result")
+    let mayor = Mayor();
+    let prom = Promedio(); 
+    document.getElementById("result").innerHTML += `
+    <h5> Nota de matematica: ${notas.matematica}</h5>
+    <h5> Nota de lengua: ${notas.lengua}</h5>
+    <h5> Nota de efsi: ${notas.efsi}</h5>
+    `;
+
+    document.getElementById("promedio").innerHTML += prom;
+    document.getElementById("alta").innerHTML += mayor;
 }
